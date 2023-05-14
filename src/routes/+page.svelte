87f0588cea3dfb0fac47b79aa1f83d64b141bb10
Miguel Gargallo <mgargallo16@gmail.com>
2023-05-14 22:09:32 +0200

    gh 0.0.1 source code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,20 +3,20 @@
 >
 	<div class="w-full max-w-5xl">
 		<div class="flex items-center justify-center mb-8">
-			<h1 class="text-4xl font-bold text-gray-900 dark:text-gray-100">Mojo Cat</h1>
+			<h1 class="text-4xl font-bold text-gray-900 dark:text-gray-100">Mojo ES</h1>
 			<span class="ml-2 text-red-500 text-2xl"
 				><img src="./favicon.gif" alt="Mojo Cat" class="w-12 h-12 ml-2" /></span
 			>
 		</div>
 		<p class="text-center text-gray-700 dark:text-gray-300 text-lg mb-12">
-			Descubre la magia del lenguaje de programación Mojo en catalán.
+			Descobreix la màgia del llenguatge de programació Mojo en l'àmbit hispà.
 		</p>
 		<div class="grid grid-cols-1 gap-8 lg:grid-cols-4">
 			<a
 				href="https://www.modular.com/mojo"
 				class="flex flex-col items-center justify-center rounded-lg border border-gray-300 px-8 py-6 transition-colors hover:border-gray-400 hover:bg-gray-50"
 			>
-				<h2 class="text-xl font-semibold text-gray-900 dark:text-gray-100 mb-2">Más información</h2>
+				<h2 class="text-xl font-semibold text-gray-900 dark:text-gray-100 mb-2">Més informació</h2>
 				<p class="text-sm text-gray-700 dark:text-gray-300">Explora sobre Modular.</p>
 			</a>
 			<a
@@ -24,21 +24,21 @@
 				class="flex flex-col items-center justify-center rounded-lg border border-gray-300 px-8 py-6 transition-colors hover:border-gray-400 hover:bg-gray-50"
 			>
 				<h2 class="text-xl font-semibold text-gray-900 dark:text-gray-100 mb-2">Pylar</h2>
-				<p class="text-sm text-gray-700 dark:text-gray-300">Potencia tu negocio.</p>
+				<p class="text-sm text-gray-700 dark:text-gray-300">Potencia el teu negoci.</p>
 			</a>
 			<a
 				href="https://mojo.li"
 				class="flex flex-col items-center justify-center rounded-lg border border-gray-300 px-8 py-6 transition-colors hover:border-gray-400 hover:bg-gray-50"
 			>
 				<h2 class="text-xl font-semibold text-gray-900 dark:text-gray-100 mb-2">Mojo ES</h2>
-				<p class="text-sm text-gray-700 dark:text-gray-300">Mojo en español.</p>
+				<p class="text-sm text-gray-700 dark:text-gray-300">Mojo en castellà.</p>
 			</a>
 			<a
 				href="https://twitter.com/miguelgargallo"
 				class="flex flex-col items-center justify-center rounded-lg border border-gray-300 px-8 py-6 transition-colors hover:border-gray-400 hover:bg-gray-50"
 			>
 				<h2 class="text-xl font-semibold text-gray-900 dark:text-gray-100 mb-2">@miguelgargallo</h2>
-				<p class="text-sm text-gray-700 dark:text-gray-300">Sobre el autor.</p>
+				<p class="text-sm text-gray-700 dark:text-gray-300">Sobre l'autor.</p>
 			</a>
 		</div>
 	</div>
