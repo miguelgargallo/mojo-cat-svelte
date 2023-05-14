97af5556a6e544694c29ad62ab3e54714173d181
Miguel Gargallo <mgargallo16@gmail.com>
2023-05-14 22:00:31 +0200

    gh 0.0.1 source code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,61 +1,45 @@
-<main class="flex min-h-screen flex-col items-center justify-between p-24">
-	<div class="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-		<p class="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 pb-6 pt-8 lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4">
-			Mojo Cat 🔥&nbsp;
-			<code class="font-mono font-bold hidden lg:inline">Aprén el nou llenguatge de programació Mojo en català.</code>
+<main
+	class="flex flex-col items-center justify-center min-h-screen p-12 bg-gray-100 dark:bg-gray-900"
+>
+	<div class="w-full max-w-5xl">
+		<div class="flex items-center justify-center mb-8">
+			<h1 class="text-4xl font-bold text-gray-900 dark:text-gray-100">Mojo Cat</h1>
+			<span class="ml-2 text-red-500 text-2xl"
+				><img src="./favicon.gif" alt="Mojo Cat" class="w-12 h-12 ml-2" /></span
+			>
+		</div>
+		<p class="text-center text-gray-700 dark:text-gray-300 text-lg mb-12">
+			Descubre la magia del lenguaje de programación Mojo en catalán.
 		</p>
-		<div class="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center lg:static lg:h-auto lg:w-auto lg:bg-none">
-			Som opensource a&nbsp;
-			<a class="text-blue-500 hover:text-blue-600" href="https://github.com/miguelgargallo/mojo-cat" target="_blank" rel="noopener noreferrer">
-				GitHub
+		<div class="grid grid-cols-1 gap-8 lg:grid-cols-4">
+			<a
+				href="https://www.modular.com/mojo"
+				class="flex flex-col items-center justify-center rounded-lg border border-gray-300 px-8 py-6 transition-colors hover:border-gray-400 hover:bg-gray-50"
+			>
+				<h2 class="text-xl font-semibold text-gray-900 dark:text-gray-100 mb-2">Más información</h2>
+				<p class="text-sm text-gray-700 dark:text-gray-300">Explora sobre Modular.</p>
+			</a>
+			<a
+				href="https://pylar.org"
+				class="flex flex-col items-center justify-center rounded-lg border border-gray-300 px-8 py-6 transition-colors hover:border-gray-400 hover:bg-gray-50"
+			>
+				<h2 class="text-xl font-semibold text-gray-900 dark:text-gray-100 mb-2">Pylar</h2>
+				<p class="text-sm text-gray-700 dark:text-gray-300">Potencia tu negocio.</p>
+			</a>
+			<a
+				href="https://mojo.li"
+				class="flex flex-col items-center justify-center rounded-lg border border-gray-300 px-8 py-6 transition-colors hover:border-gray-400 hover:bg-gray-50"
+			>
+				<h2 class="text-xl font-semibold text-gray-900 dark:text-gray-100 mb-2">Mojo ES</h2>
+				<p class="text-sm text-gray-700 dark:text-gray-300">Mojo en español.</p>
+			</a>
+			<a
+				href="https://twitter.com/miguelgargallo"
+				class="flex flex-col items-center justify-center rounded-lg border border-gray-300 px-8 py-6 transition-colors hover:border-gray-400 hover:bg-gray-50"
+			>
+				<h2 class="text-xl font-semibold text-gray-900 dark:text-gray-100 mb-2">@miguelgargallo</h2>
+				<p class="text-sm text-gray-700 dark:text-gray-300">Sobre el autor.</p>
 			</a>
 		</div>
 	</div>
-	<div class="py-8 relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:content-[''] before:lg:h-[360px]">
-		<h1 class="relative z-10 text-6xl font-bold text-center text-gray-900 lg:text-8xl">
-			<span class="block">Aviat</span>
-			<span class="block">Online</span>
-		</h1>
-	</div>
-	<div class="py-8 mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-		<a href="https://www.modular.com/mojo" class="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100" target="_blank" rel="noopener noreferrer">
-			<h2 class="mb-3 text-2xl font-semibold">
-				Llegeix mes 🔥
-				<span class="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-					-&gt;
-				</span>
-			</h2>
-			<p class="m-0 max-w-[30ch] text-sm opacity-50">Descobreix el lloc oficial de Modular.</p>
-		</a>
-		<a href="https://pylar.org" class="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100" target="_blank" rel="noopener noreferrer">
-			<h2 class="mb-3 text-2xl font-semibold">
-				Pylar
-				<span class="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-					-&gt;
-				</span>
-			</h2>
-			<p class="m-0 max-w-[30ch] text-sm opacity-50">Integrem Pylar
-				AI verticalment al teu negoci.
-				</p>
-			</a>
-			<a href="https://mojo.li" class="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100">
-				<h2 class="mb-3 text-2xl font-semibold">
-					Mojo ES 🔥
-					<span class="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-						-&gt;
-					</span>
-				</h2>
-				<p class="m-0 max-w-[30ch] text-sm opacity-50">Mojo en espanyol.</p>
-			</a>
-			<a href="https://twitter.com/miguelgargallo" class="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100" target="_blank" rel="noopener noreferrer">
-				<h2 class="mb-3 text-2xl font-semibold">
-					@miguelgargallo
-					<span class="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-						-&gt;
-					</span>
-				</h2>
-				<p class="m-0 max-w-[30ch] text-sm opacity-50">Mojo Cat. 2023 Tots els drets reservats. mojo.cat no és un lloc oficial ni afiliat a Modular.</p>
-			</a>
-		</div>
-	</main>
-	
+</main>
